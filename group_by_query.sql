0594bb3aba859b88bb89a0b2b903e8d900fc411a
SaraPasinato <pasinatosara.sp@gmail.com>
2021-09-28 14:45:47 +0200

    add 3 query >> group_by

--- group_by_query.sql
+++ group_by_query.sql
@@ -10,3 +10,10 @@ SELECT COUNT(*) AS `tot_insegnanti`,`office_address`
 FROM `teachers`
 GROUP BY `office_address`;
 
+--! 3) Calcolare la media dei voti di ogni appello d'esame
+SELECT ROUND(AVG(`vote`),2) AS `avg`, `exam_id`
+FROM `exam_student`
+GROUP BY `exam_id`;
+
+--! 4) Contare quanti corsi di laurea ci sono per ogni dipartiemnto
+
