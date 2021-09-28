d0ad2c3f1ffbbab71a1205057deb7b7bf49e06e2
SaraPasinato <pasinatosara.sp@gmail.com>
2021-09-28 14:37:46 +0200

    add 2 query >> group_by

--- group_by_query.sql
+++ group_by_query.sql
@@ -4,3 +4,9 @@ SELECT count(`year`) as `n_iscritti`, `year`
 FROM `courses`
 GROUP BY `year`;
 
+--! 2) Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+SELECT COUNT(*) AS `tot_insegnanti`,`office_address`
+FROM `teachers`
+GROUP BY `office_address`;
+
