1d0ea7695ac267c16f246d8924828a785121eca4
SaraPasinato <pasinatosara.sp@gmail.com>
2021-09-29 14:25:48 +0200

    add 3 query

--- join_query.sql
+++ join_query.sql
@@ -9,3 +9,5 @@ SELECT `degrees`.`name`,`departments`.`name`
 FROM `degrees`
 JOIN `departments` ON `degrees`.`department_id`=`departments`.`id`
 WHERE `departments`.`name`='Dipartimento di Neuroscienze';
+
+-- ! 3) Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
