b08c9345d16162ba3f5889a5d3207431853b2fa7
SaraPasinato <pasinatosara.sp@gmail.com>
2021-09-29 14:12:23 +0200

    add 2 query

--- join_query.sql
+++ join_query.sql
@@ -3,3 +3,9 @@ SELECT `students`.`name`,`students`.`surname`,`degrees`.`name`
 FROM `students`
 JOIN `degrees` ON `students`.`degree_id`=`degrees`.`id`
 WHERE `degrees`.`name`='Corso di Laurea in Economia';
+
+-- ! 2) Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+SELECT `degrees`.`name`,`departments`.`name`
+FROM `degrees`
+JOIN `departments` ON `degrees`.`department_id`=`departments`.`id`
+WHERE `departments`.`name`='Dipartimento di Neuroscienze';
