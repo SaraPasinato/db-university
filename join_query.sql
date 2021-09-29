13e75e9d368e7ea4feb02bf4af7936760dc9513d
SaraPasinato <pasinatosara.sp@gmail.com>
2021-09-29 14:44:05 +0200

    add 6 query

--- join_query.sql
+++ join_query.sql
@@ -31,3 +31,13 @@ FROM `degrees`
 JOIN `courses` ON `degrees`.`id`=`courses`.`degree_id`
 JOIN `course_teacher` ON `courses`.`id`=`course_teacher`.`course_id`
 JOIN `teachers` ON `course_teacher`.`teacher_id`= `teachers`.`id`;
+
+-- ! 6)Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+
+SELECT `teachers`.`name`, `teachers`.`surname`,`departments`.`name`
+FROM `teachers`
+JOIN `course_teacher` ON `teachers`.`id`=`course_teacher`.`teacher_id`
+JOIN `courses` ON `course_teacher`.`course_id`=`courses`.`id`
+JOIN `degrees` ON `courses`.`degree_id`=`degrees`.`id`
+JOIN `departments`ON `degrees`.`department_id`=`departments`.`id`
+WHERE `departments`.`name`='Dipartimento di Matematica';
